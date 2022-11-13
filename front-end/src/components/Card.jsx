0694a3b3d08bd0fcff1b86e2cc8b067817df4190
10Dev <today13113@gmail.com>
2022-11-14 00:14:43 +0700

    update

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+import Select from 'react-select';
+
+
+const optionSS = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+  
+  const SelectComponent=(options)=> {
+    const [selectedOption, setSelectedOption] = useState(null);
+  
+    return (
+      <div className="select">
+        <Select
+          value={selectedOption}
+          onChange={setSelectedOption}
+          options={options}
+        />
+      </div>
+    );
+  }
+
+
+
 
 function Card() {
+  let [text, setText] = useState(null);
+  let roomType = ["พัดลม", "แอร์", "สูท"];
+  let gender = ["ชาย", "หญิง", "รวม"];
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+  }
   return (
-    <div>Card</div>
-  )
+    <div
+      className="card"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "60%",
+        height: "75%",
+        boxSizing: "border-box",
+        border: "5px solid #7DEAE7",
+        borderRadius: "15px",
+      }}
+    >
+      <form onSubmit={handleSubmit}>
+        <label>
+            {SelectComponent(optionSS)}
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
