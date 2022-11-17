737e6b16cb3801185c505d80175876ca8d038baf
10Dev <today13113@gmail.com>
2022-11-17 23:42:25 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,30 +8,21 @@ gender = st.selectbox(
 
     'Select the gender?',
 
-    ('รวม','หญิง','ชาย'))
+    ('รวม', 'หญิง', 'ชาย'))
 room = st.selectbox(
 
     'Select the Room type?',
 
-    ('แอร์','พัดลม','สูท'))
+    ('แอร์', 'พัดลม', 'สูท'))
 price = st.slider('Price?', 2000, 12000, 4000)
 distance = st.slider('Distance?', 900, 3000, 1000)
 # distanceCheck = st.checkbox('I agree')
-if st.button('คำนวณ'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
-#displaying the selected option
+
+# displaying the selected option
 col1, col2, col3 = st.columns(3)
 
-# with col1:
-#    st.header("A cat")
-#    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-   st.header("A dog")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
-
-# with col3:
-#    st.header("An owl")
-#    st.image("https://static.streamlit.io/examples/owl.jpg")
+with col3:
+    if st.button('คำนวณ'):
+        st.write('Why hello there')
+    else:
+        st.write('Goodbye')
