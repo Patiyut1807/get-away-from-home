859612cb5278163f5ef4b8d610c0be568fa125d6
10Dev <today13113@gmail.com>
2022-11-17 22:52:39 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-st.write("Here's our first attempt at using data to create a table:")
-choice = st.selectbox(
+
+st.header("เบื่อแล้วอยู่บ้าน อยู่หอดีกว่า: 👏")
+st.write("เว็บแอพสำหรับช่วยในการตัดสินใจเลือกหอพัก")
+gender = st.selectbox(
+
+    'Select the gender?',
+
+    ('รวม','หญิง','ชาย'))
+room = st.selectbox(
 
     'Select the Room type?',
 
-    ('ชาย','หญิง','รวม'))
-
-
+    ('แอร์','พัดลม','สูท'))
+price = st.slider('Price?', 2000, 12000, 4000)
+distance = st.slider('Distance?', 900, 3000, 1000)
+# distanceCheck = st.checkbox('I agree')
 
 #displaying the selected option
-
-st.write('You have selected:', choice)
